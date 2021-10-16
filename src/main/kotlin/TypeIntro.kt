32a97e4902d246ca992d46c25d2d99dff1a6a086
RMSAJ <92260209+RMSAJ@users.noreply.github.com>
2021-10-16 05:28:40 +0300

    Initial

---
+++ src/main/kotlin/TypeIntro.kt
@@ -0,0 +1,26 @@
+const val MAX_EXPERIENCE = 5000
+
+fun main() {
+    var experiencePonits = 5
+    // the exp of the player in the game
+    val playerName = "Rakan"
+    // the player entry name at beginnig of the game
+    var hasSteed : Boolean = true
+    var location = " The Unicorn's Horn"
+    var playerMoney = 50
+    var puplican = "Magy"
+    val menu = "Soda,tea,LaCroix"
+    // does he has a dragon to mount or not
+    experiencePonits += 8
+    println("player exp: $experiencePonits")
+    println("playerName: ${playerName.reversed()} ")
+    println("Player-money:  $playerMoney")
+    println("the locantion of the player $location")
+    println("the seller $puplican ")
+    println(" the menu for the player   $menu")
+
+}
+
+
+
+
